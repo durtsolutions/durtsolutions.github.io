0473ae956e5673c606cd2f4cd39ba248fafa5d4d
DURT Solutions <79685058+durtsolutions@users.noreply.github.com>
2021-02-27 17:14:47 +0800

    Update contactengine.php

--- contactengine.php
+++ contactengine.php
@@ -6,6 +6,8 @@ $Subject = "Cleaning Inquiry | Route Website";
 $Name = Trim(stripslashes($_POST['Name'])); 
 $Tel = Trim(stripslashes($_POST['Tel'])); 
 $Email = Trim(stripslashes($_POST['Email'])); 
+$Mobile = Trim(stripslashes($_POST['Mobile'])); 
+$City = Trim(stripslashes($_POST['City'])); 
 $Message = Trim(stripslashes($_POST['Message'])); 
 
 // validation
@@ -35,7 +37,7 @@ $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
 
 // redirect to success page 
 if ($success){
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=https://durtsolutions.github.io\">";
 }
 else{
   print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
